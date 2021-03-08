897dd4f242b46bb936486000289d49d0592d064d
Paige Bowman <paigeelisabethbowman@gmail.com>
2021-03-08 10:21:20 -0600

    Initial Commit

---
+++ Za' Hunter Outline/Za__Hunter_OutlineApp.swift
@@ -0,0 +1,17 @@
+//
+//  Za__Hunter_OutlineApp.swift
+//  Za' Hunter Outline
+//
+//  Created by Student on 3/8/21.
+//
+
+import SwiftUI
+
+@main
+struct Za__Hunter_OutlineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
