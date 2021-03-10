f065a094fea57131797568a4ed54b6421a594ab8
Paige Bowman <paigeelisabethbowman@gmail.com>
2021-03-10 10:26:07 -0600

    Added pizza icon and link to store website

--- Za' Hunter Outline/ContentView.swift
+++ Za' Hunter Outline/ContentView.swift
@@ -18,13 +18,38 @@ struct ContentView: View {
         span: MKCoordinateSpan(
             latitudeDelta: 0.05,
             longitudeDelta: 0.05))
+    @State private var places = [Place]()
     var body: some View {
         Map(
             coordinateRegion: $region,
             interactionModes: .all,
             showsUserLocation: true,
-            userTrackingMode: $userTrackingMode
-        )
+            userTrackingMode: $userTrackingMode,
+            annotationItems: places) { place in
+            MapAnnotation(coordinate: place.annotation.coordinate) {
+                Marker(mapItem: place.mapItem)
+            }
+        }
+        .onAppear(perform: {
+            performSearch(item: "Pizza")
+        })
+    }
+    
+    func performSearch(item: String) {
+        let searchRequest = MKLocalSearch.Request()
+        searchRequest.naturalLanguageQuery = item
+        searchRequest.region = region
+        let search = MKLocalSearch(request: searchRequest)
+        search.start { (response, error) in
+            if let response = response {
+                for mapItem in response.mapItems {
+                    let annotation = MKPointAnnotation()
+                    annotation.coordinate = mapItem.placemark.coordinate
+                    annotation.title = mapItem.name
+                    places.append(Place(annotation: annotation, mapItem: mapItem))
+                }
+            }
+        }
     }
 }
 
@@ -33,3 +58,21 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+struct Place: Identifiable {
+    let id = UUID()
+    let annotation: MKPointAnnotation
+    let mapItem: MKMapItem
+}
+
+struct Marker: View {
+    var mapItem: MKMapItem
+    var body: some View {
+        if let url = mapItem.url {
+            Link(destination: url, label: {
+                Image("pizza")
+            })
+        }
+    }
+}
+
--- Za' Hunter Outline/LocationManager.swift
+++ Za' Hunter Outline/LocationManager.swift
@@ -10,6 +10,7 @@ import CoreLocation
 
 class LocationManager: NSObject, CLLocationManagerDelegate, ObservableObject {
     var locationManager = CLLocationManager()
+    
     override init() {
         super.init()
         locationManager.delegate = self
